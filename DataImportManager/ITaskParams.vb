2906806e3c0c5268d55748b4295044b690c60d68
alchemistmatt <matthew.monroe@pnnl.gov>
2016-11-18 13:29:18 -0800

    Tabs to spaces

--- DataImportManager/ITaskParams.vb
+++ DataImportManager/ITaskParams.vb
@@ -1,9 +1,9 @@
 Public Interface ITaskParams
-	' Used for job closeout
-	Enum CloseOutType
-		CLOSEOUT_SUCCESS = 0
-		CLOSEOUT_FAILED = 1
+    ' Used for job closeout
+    Enum CloseOutType
+        CLOSEOUT_SUCCESS = 0
+        CLOSEOUT_FAILED = 1
         CLOSEOUT_NO_DATA = 10
-	End Enum
+    End Enum
 
 End Interface
