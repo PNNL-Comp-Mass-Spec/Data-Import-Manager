b9416f7708005629af667d8dba970e09fc9d545b
Matthew Monroe <matt@alchemistmatt.com>
2018-03-25 14:29:43 -0700

    Remove unused classes

--- DataImportManager/IDataImportResources.cs
+++
@@ -1,9 +0,0 @@
-Imports PRISM.Logging
-
-'Public Interface IDataImportResources
-
-'ReadOnly Property Message() As String
-
-'Function GetResources() As ITaskParams.CloseOutType
-
-'End Interface
--- DataImportManager/ITaskParams.cs
+++
@@ -1,9 +0,0 @@
-Public Interface ITaskParams
-    ' Used for job closeout
-    Enum CloseOutType
-        CLOSEOUT_SUCCESS = 0
-        CLOSEOUT_FAILED = 1
-        CLOSEOUT_NO_DATA = 10
-    End Enum
-
-End Interface
