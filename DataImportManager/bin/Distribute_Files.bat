89eb4ebe062e311d982349c4b91fa978f32dc7b6
alchemistmatt <matthew.monroe@pnnl.gov>
2018-11-19 13:48:18 -0800

    Add batch file

---
+++ DataImportManager/bin/Distribute_Files.bat
@@ -0,0 +1,10 @@
+
+xcopy Release\DataImportManager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
+xcopy Release\DataImportManager.pdb \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
+xcopy Release\*.dll \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
+
+xcopy Release\DataImportManager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
+xcopy Release\DataImportManager.pdb \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
+xcopy Release\*.dll \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
+
+pause
