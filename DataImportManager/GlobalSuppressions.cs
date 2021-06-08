cdde37950043cdfa57ed11ee1bece343a8ca3912
alchemistmatt <matthew.monroe@pnnl.gov>
2021-06-08 10:07:13 -0700

    Suppress warnings

--- DataImportManager/GlobalSuppressions.cs
+++ DataImportManager/GlobalSuppressions.cs
@@ -6,7 +6,13 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Readability", "RCS1123:Add parentheses when necessary.", Justification = "Parentheses not needed", Scope = "member", Target = "~P:DataImportManager.ThreadSafeRandom.ThisThreadsRandom")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allow legacy name", Scope = "type", Target = "~T:DataImportManager.clsDataImportTask")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allow legacy name", Scope = "type", Target = "~T:DataImportManager.clsDBTask")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allow legacy name", Scope = "type", Target = "~T:DataImportManager.clsGlobal")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allow legacy name", Scope = "type", Target = "~T:DataImportManager.clsLoggerBase")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allow legacy name", Scope = "type", Target = "~T:DataImportManager.clsMainProcess")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allow legacy name", Scope = "type", Target = "~T:DataImportManager.clsProcessXmlTriggerFile")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allow legacy name", Scope = "type", Target = "~T:DataImportManager.clsQueuedMail")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allow legacy name", Scope = "type", Target = "~T:DataImportManager.clsValidationError")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allow legacy name", Scope = "type", Target = "~T:DataImportManager.clsValidationErrorSummary")]
 [assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Allow legacy name", Scope = "type", Target = "~T:DataImportManager.clsXMLTimeValidation")]
