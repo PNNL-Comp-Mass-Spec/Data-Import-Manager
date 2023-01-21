dae714673bd9a00c19b898d5278f18e92fb075cb
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-21 15:33:16 -0800

    Update default settings

--- DataImportManager/Properties/Settings.Designer.cs
+++ DataImportManager/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace DataImportManager.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -44,7 +44,7 @@ namespace DataImportManager.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Proto-3_DIM")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Proto-6_DIM")]
         public string MgrName {
             get {
                 return ((string)(this["MgrName"]));
