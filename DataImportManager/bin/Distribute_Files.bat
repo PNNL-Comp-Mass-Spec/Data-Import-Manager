2f1a30a97673d7f152946f506f2274d5d202bcde
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2025-07-14 12:01:44 -0700

    Convert the project to  dual-compile against .NET 4.8 and .NET 8.0

--- DataImportManager/bin/Distribute_Files.bat
+++ DataImportManager/bin/Distribute_Files.bat
@@ -1,10 +1,10 @@
 
-xcopy Debug\DataImportManager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
-xcopy Debug\DataImportManager.pdb \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
-xcopy Debug\*.dll \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
+xcopy Debug\net48\DataImportManager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
+xcopy Debug\net48\DataImportManager.pdb \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
+xcopy Debug\net48\*.dll \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
 
-xcopy Debug\DataImportManager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
-xcopy Debug\DataImportManager.pdb \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
-xcopy Debug\*.dll \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
+xcopy Debug\net48\DataImportManager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
+xcopy Debug\net48\DataImportManager.pdb \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
+xcopy Debug\net48\*.dll \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
 
 if not "%1"=="NoPause" pause
