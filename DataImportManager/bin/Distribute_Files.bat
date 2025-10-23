525dec0b5922ef5a81c42dfa4fbfc53f79157425
alchemistmatt <matthew.monroe@pnnl.gov>
2025-10-22 18:03:21 -0700

    Update batch distribution file

--- DataImportManager/bin/Distribute_Files.bat
+++ DataImportManager/bin/Distribute_Files.bat
@@ -1,10 +1,10 @@
 
-xcopy Debug\net48\DataImportManager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
-xcopy Debug\net48\DataImportManager.pdb \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
-xcopy Debug\net48\*.dll \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
+xcopy Debug\net48\DataImportManager.exe \\Proto-3\DMS_Programs_Dist\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
+xcopy Debug\net48\DataImportManager.pdb \\Proto-3\DMS_Programs_Dist\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
+xcopy Debug\net48\*.dll \\Proto-3\DMS_Programs_Dist\CaptureTaskManagerDistribution\DataImportManager\ /Y /D
 
-xcopy Debug\net48\DataImportManager.exe \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
-xcopy Debug\net48\DataImportManager.pdb \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
-xcopy Debug\net48\*.dll \\pnl\projects\OmicsSW\DMS_Programs\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
+xcopy Debug\net48\DataImportManager.exe \\Proto-3\DMS_Programs_Dist\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
+xcopy Debug\net48\DataImportManager.pdb \\Proto-3\DMS_Programs_Dist\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
+xcopy Debug\net48\*.dll \\Proto-3\DMS_Programs_Dist\CaptureTaskManagerDistribution\DataImportManagerMan\ /Y /D
 
 if not "%1"=="NoPause" pause
